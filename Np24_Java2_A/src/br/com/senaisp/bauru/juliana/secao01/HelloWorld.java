ad9aae608096d6596364a4c32679215cd370ef1b
julianabrunes <julianabrunespimente@gmail.com>
2024-03-14 19:18:19 -0300

    commit inicial

---
+++ Np24_Java2_A/src/br/com/senaisp/bauru/juliana/secao01/HelloWorld.java
@@ -0,0 +1,10 @@
+package br.com.senaisp.bauru.juliana.secao01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");	
+
+	}
+
+}
---
+++ Np24_Java2_A/src/br/com/senaisp/bauru/juliana/secao01/RevisaoParametro.java
@@ -0,0 +1,10 @@
+package br.com.senaisp.bauru.juliana.secao01;
+
+public class RevisaoParametro {
+	
+    public static void main(String[] args) {
+       for(String item : args) {
+	System.out.println(item);
+       }
+    }
+}
